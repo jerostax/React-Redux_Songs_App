47a578bcf1140f21ab15bcf40c7b189762aa984b
jerostax <jeremy.geneste@gmail.com>
2019-05-13 17:24:38 +0200

    [ FEATURE ] Rendered songs titles by map() on props.songs and added some style

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,10 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends React.Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">
+              Selectioner
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
   render() {
-    console.log(this.props)
     return (
-      <div>SongList</div>
+      <div className="ui divided list">{this.renderList()}</div>
     );
   }
 }
@@ -13,7 +26,7 @@ class SongList extends React.Component {
 const mapStateToProps = state => {
 
   return { songs: state.songs };
-}
+};
 
 export default connect(mapStateToProps)(SongList);
 
