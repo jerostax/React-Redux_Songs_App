9c3872e359901c5c467c5db305267651c4c213ed
jerostax <jeremy.geneste@gmail.com>
2019-05-13 16:17:02 +0200

    [ CONFIG ] Added songsReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+  return [
+    { title: 'Wish you were here', duration: '4:05'},
+    { title: 'Starway to heaven', duration: '7:05'},
+    { title: 'Let it be', duration: '3:35'},
+    { title: 'Garota de ipanema', duration: '4:35'}
+  ];
+};
